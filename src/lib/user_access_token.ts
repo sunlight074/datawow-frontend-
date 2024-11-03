8061522ae7731972999709b6b981f422dc000311
worawut <worawut48@hotmail.co.th>
2024-11-03 21:43:46 +0700

    ci: rename cookieStore

--- src/lib/user_access_token.ts
+++ src/lib/user_access_token.ts
@@ -3,10 +3,10 @@ import { cookies } from "next/headers";
 
 export async function setUserAccessToken(token: string) {
 	const cookieStore = await cookies();
-	cookieStore.set("token", token);
+	cookieStore.set("user_access_token", token);
 }
 
 export async function getUserAccessToken() {
 	const cookieStore = await cookies();
-	return cookieStore.get("token")?.value;
+	return cookieStore.get("user_access_token")?.value;
 }
