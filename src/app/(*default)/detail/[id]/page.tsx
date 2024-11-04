3510ee6c3d71f1b2b77fa7cb4fc1198cf5bb8180
Dont <79631484+sunlight074@users.noreply.github.com>
2024-11-04 21:43:21 +0700

    Update page.tsx

--- src/app/(*default)/detail/[id]/page.tsx
+++ src/app/(*default)/detail/[id]/page.tsx
@@ -22,7 +22,7 @@ export default async function Page(props: { params: tParams }) {
 					result={{
 						blog_id: "1",
 						blog_title: "Test Blog",
-						blog_description: "Test Blog Description",
+						blog_description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 						community_id: "1",
 						community_name: "Test Community",
 						comment_count: 5,
