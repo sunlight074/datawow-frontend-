86f15a91181f1cde7bc6554713163615e5f91821
worawut <worawut48@hotmail.co.th>
2024-11-04 21:28:26 +0700

    ci: update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
 import DrawerCustom from "./Drawer";
-export default function Navbar() {
+import { getUserAccessToken } from "@/lib/user_access_token";
+export default async function Navbar() {
+	const userAccessToken = await getUserAccessToken();
+
 	return (
 		<nav className="bg-primary-green-500 h-[60px] w-full flex space-x-3 items-center px-5 text-primary-white">
 			<div className="italic font-normal flex-1 ">a Board</div>
-			<div className="flex space-x-3 items-center">
-				<p>name</p>
-				<Image
-					src={"/images/profile.jpeg"}
-					alt="profile"
-					className="rounded-full object-contain"
-					width={30}
-					height={30}
-				/>
-			</div>
+			{userAccessToken && (
+				<div className="flex space-x-3 items-center">
+					<p>{userAccessToken}</p>
+					<Image
+						src={"/images/profile.jpeg"}
+						alt="profile"
+						className="rounded-full object-contain"
+						width={30}
+						height={30}
+					/>
+				</div>
+			)}
 			<div className="block lg:hidden">
 				<DrawerCustom />
 			</div>
-			<Link
-				href="/login"
-				className="bg-primary-success w-fit h-10 px-4 py-3 rounded-md lg:flex items-center font-semibold hover:underline hidden"
-			>
-				sing in
-			</Link>
+			{userAccessToken ? (
+				<Link
+					href="/api/logout"
+					className="bg-red-500 w-fit h-10 px-4 py-3 rounded-md lg:flex items-center font-semibold hover:underline hidden"
+				>
+					sing out
+				</Link>
+			) : (
+				<Link
+					href="/login"
+					className="bg-primary-success w-fit h-10 px-4 py-3 rounded-md lg:flex items-center font-semibold hover:underline hidden"
+				>
+					sing in
+				</Link>
+			)}
 		</nav>
 	);
 }
